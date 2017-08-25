9f0875dc7d4d3b6b4e6ad3e52cb07a26edf727a9
omarmhmmd <omar.mhmmd@gmail.com>
2017-08-25 16:49:44 -0700

    sideways scrolling 016

--- js/sfmomabathrooms.js
+++ js/sfmomabathrooms.js
@@ -7,11 +7,8 @@ $(function() {
 
     /******** MOBILE ********/
     if($(window).width() < 481) {
-      
-      $(document).bind("touchmove", function(e){
-        e.preventDefault();
-      });
 
+  
       console.log("phone");
       //$('.broom').css('background-color', 'red');
 
