b186f23a5cde2a0ed9d3838f81843325c84d91ba
omarmhmmd <omar.mhmmd@gmail.com>
2017-09-03 17:24:19 -0700

    fix level

--- js/sfmomabathrooms.js
+++ js/sfmomabathrooms.js
@@ -82,6 +82,7 @@ $(function() {
    }
 
     $(".zineButton").click(function(){
+      $(".level").css('visibility', 'hidden');
       if($(".zinePic").is(":visible")) {
         setTimeout(function () {
           $(".broom").slideToggle(750);}, 800
